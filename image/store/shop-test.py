198b714a2d733881fa337eb720384d63a939ffad
root <thomasjou@gmail.com>
2021-09-25 16:08:17 +0200

    change shop python

--- image/store/shop-test.py
+++ image/store/shop-test.py
@@ -1,9 +1,11 @@
+import threading
 import os
 import socket
-import time
 
-while True:
+def print_advertising():
   hostname = socket.gethostname()
   item = os.environ.get('STORE_ITEM_KEY')
+  threading.Timer(1.0, print_advertising).start()
   print ("I'm the shop {} and i sell {}".format(hostname, item))
-  time.sleep(1)
+
+print_advertising()
